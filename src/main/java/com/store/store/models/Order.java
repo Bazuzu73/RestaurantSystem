038c58f8623a93f67673d8bccaaf7832f83b8cee
szryachev <bazuzu73@outlook.com>
2023-12-29 20:13:10 -0500

    TASK-1 : Update Order model

--- src/main/java/com/store/store/models/Order.java
+++ src/main/java/com/store/store/models/Order.java
@@ -11,8 +11,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Order_Table")
 public class Order {
 
     @Id
