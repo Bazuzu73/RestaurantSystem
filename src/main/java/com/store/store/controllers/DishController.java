2259d79bacf0bc8dc9330ff74db3eaad2f724129
Bazuzu73 <bazuzu73@outlook.com>
2024-01-08 10:03:46 -0500

    TASK-5 : Refactor dish update

--- src/main/java/com/store/store/controllers/DishController.java
+++ src/main/java/com/store/store/controllers/DishController.java
@@ -10,12 +10,16 @@ import org.springframework.ui.Model;
 import com.store.store.models.Dish;
 import com.store.store.models.Ingredient;
 import com.store.store.services.DishService;
+import com.store.store.services.IngredientService;
 
 @Controller
 public class DishController {
 
     @Autowired
     private DishService dishService;
+
+    @Autowired
+    private IngredientService ingredientService;
 
     @GetMapping("/dishes")
     public String getListofDishes(Model model) {
@@ -24,15 +28,17 @@ public class DishController {
         return "dishes";
     }
 
-    @GetMapping("/dish/{id}")
+    @GetMapping("/dish/{id}/update")
     public String getDish(@PathVariable(value = "id") int id, Model model) {
         if (!dishService.ifExist(id)) {
             return "redirect:/dishes";
         }
-        List<Dish> dish = dishService.getById(id);
-        List<Ingredient> ingredients = dishService.getRelated(id);
+        Dish dish = dishService.getById(id).get(0);
+        List<Ingredient> dishIngredients = dishService.getRelated(id);
+        Iterable<Ingredient> allIngredients = ingredientService.getAll();
         model.addAttribute("dish", dish);
-        model.addAttribute("ingredients", ingredients);
+        model.addAttribute("ingredients", dishIngredients);
+        model.addAttribute("allIngredients", allIngredients);
         return "dishDetailed";
     }
 }
